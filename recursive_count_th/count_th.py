ec65d26f3944f1b9c88a73d40bf106bbb4f1d6e4
mtoce <mtoce@villanova.edu>
2020-07-24 11:59:01 -0400

    finished recursive_count_th

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -4,7 +4,21 @@ Your function should return a count of how many occurences of ***"th"*** occur w
 Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
+    count = 0
+
+    # catch empty words
+    if not word:
+        return 0
+    # catch single letter words
+    if len(word) == 1:
+        return 0
     
-    # TBC
+    #recurse down the word
+    if len(word) > 1:
+        count += count_th(word[1:])
     
-    pass
+    # find 'th's
+    if word[:2] == 'th':
+        count += 1
+    
+    return count
